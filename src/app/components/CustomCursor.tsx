828d623886512f5229c66465d37996eaf7b3ca9f
Ali Aslam <aliaslam@Mac.lan>
2026-02-02 00:24:46 +0400

    Update cursor to use image file instead of SVG

--- src/app/components/CustomCursor.tsx
+++ src/app/components/CustomCursor.tsx
@@ -68,46 +68,12 @@ export default function CustomCursor() {
       }}
     >
       <div className="w-10 h-10 md:w-12 md:h-12">
-        <ToothLogo />
+        <img 
+          src="/tooth-logo.png" 
+          alt="Sky Dental Logo" 
+          className="w-full h-full object-contain"
+        />
       </div>
     </div>
-  )
-}
-
-function ToothLogo() {
-  return (
-    <svg 
-      viewBox="0 0 100 100" 
-      className="w-full h-full"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {/* Single continuous dark blue line - starts bottom left, curves up to crown, intertwines in middle, forms roots */}
-      <path 
-        stroke="#0C0060" 
-        strokeWidth="3" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
-        fill="none"
-        d="M28 92 
-           Q22 88, 20 82
-           Q18 72, 22 58
-           Q26 44, 32 34
-           Q36 28, 42 25
-           Q45 23, 48 22
-           Q50 21, 52 22
-           Q55 23, 58 25
-           Q64 28, 68 34
-           Q74 44, 78 58
-           Q82 72, 80 82
-           Q78 88, 72 92
-           Q68 90, 64 92
-           Q60 90, 56 92
-           Q52 90, 48 92
-           Q44 90, 40 92
-           Q36 90, 32 92
-           Q30 92, 28 92"
-      />
-    </svg>
   )
 }
