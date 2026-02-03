f9e1861e1a452f716cb97a7a8658b90a7bcafaa9
Ali Aslam <aliaslam@Alis-MacBook-Pro-2.local>
2026-02-03 14:52:53 +0400

    Increase service icon size from w-8 h-8 to w-12 h-12

--- src/app/components/ServicesSection.tsx
+++ src/app/components/ServicesSection.tsx
@@ -75,7 +75,7 @@ function ServiceCard({ service, index }: { service: typeof services[0]; index: n
         className="w-16 h-16 rounded-2xl flex items-center justify-center mb-6 relative z-10"
         style={{ backgroundColor: service.bgColor }}
       >
-        <service.Icon className="w-8 h-8" />
+        <service.Icon className="w-12 h-12" />
       </div>
 
       {/* Content */}
