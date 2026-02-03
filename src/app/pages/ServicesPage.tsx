09be4f6c915dde62bffce9afc160ffdbb50e5298
Ali Aslam <aliaslam@Mac.lan>
2026-02-03 19:47:41 +0400

    Hardcode service icon background color to #56FC8A

--- src/app/pages/ServicesPage.tsx
+++ src/app/pages/ServicesPage.tsx
@@ -91,7 +91,7 @@ function ServiceCard({ service, index }: { service: typeof services[0]; index: n
       {/* Icon */}
       <div 
         className="w-16 h-16 rounded-2xl flex items-center justify-center mb-6 relative z-10"
-        style={{ backgroundColor: service.bgColor }}
+        style={{ backgroundColor: '#56FC8A' }}
       >
         <service.Icon className="w-[70px] h-[70px]" />
       </div>
